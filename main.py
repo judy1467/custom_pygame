7586ca2b3e31e8c6d474a52465433a8d5b81884f
judy1 <judy1467@naver.com>
2022-08-13 00:17:13 +0900

    이미지 외곽선 추출 & 리사이징, 화살표 움직임 추가

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ import pygame as pg
 import pygame.font
 
 pg.init() # 초기화
+
 width, height = 600, 600
 default_color = (255, 255, 255)
-
 screen = pg.display.set_mode((width, height))
 pg.display.set_caption('빛의 경로')
 font = pygame.font.SysFont("arial", 20, False, False)
 text = font.render("times: ", True, default_color)
-clock = pygame.time.Clock() #프레임 설 정
+clock = pygame.time.Clock() #프레임 설정
 arrow = pygame.image.load("resources/images/arrow.png")
+map = pygame.image.load("resources/convert/map1.jpg")
+keys = [False, False, False, False]
+playerpos = [100, 100]
 
 while True:
     screen.fill((0,0,0)) # 화면 초기화
     clock.tick(30) # 30프레임
-    pg.draw.rect(screen, default_color, (10, 35, 580, 555), 2) # 직사각형 그리기
-    screen.blit(arrow, (100, 100))
+    pg.draw.rect(screen, default_color, (10, 35, 580, 555), 2) # 직사각형 그리기 width = 570, height = 520
+    screen.blit(map, (10+2, 35+2))
+    screen.blit(arrow, playerpos)
     screen.blit(text, (10, 10))
 
     pg.display.update() # 화면 전체 업데이트 메소드
@@ -28,7 +32,33 @@ while True:
     #pg.draw.polygon(screen, default_color, ((100, 100), (0, 200), (200, 200)), 5) # 다각형 그리기
 
     for event in pg.event.get():
-
         if event.type == pg.QUIT:
             pg.quit() # pygame 종료
             exit(0) # while문 종료
+        if event.type == pg.KEYDOWN: # 키다운 감지
+            if event.key == pg.K_w:
+                keys[0] = True
+            elif event.key == pg.K_a:
+                keys[1] = True
+            elif event.key == pg.K_s:
+                keys[2] = True
+            elif event.key == pg.K_d:
+                keys[3] = True
+        if event.type == pg.KEYUP: # 키업 감지
+            if event.key == pg.K_w:
+                keys[0] = False
+            elif event.key == pg.K_a:
+                keys[1] = False
+            elif event.key == pg.K_s:
+                keys[2] = False
+            elif event.key == pg.K_d:
+                keys[3] = False
+
+    if keys[0]:
+        playerpos[1] = playerpos[1] - 5
+    elif keys[2]:
+        playerpos[1] = playerpos[1] + 5
+    if keys[1]:
+        playerpos[0] = playerpos[0] - 5
+    elif keys[3]:
+        playerpos[0] = playerpos[0] + 5
--- test.py
+++ test.py
@@ -1,17 +1,26 @@
 import cv2, os
-
-image_gray = cv2.imread('resources/images/map/map1.jpg', cv2.IMREAD_GRAYSCALE)
+from PIL import Image
 
 file_list = os.listdir('resources/images/map/')
 print(file_list)
 
-blur = cv2.GaussianBlur(image_gray, ksize=(3,3), sigmaX=0)
-ret, thresh1 = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
-edged = cv2.Canny(blur, 10, 250)
+for map in file_list:
+    image_gray = cv2.imread('resources/images/map/'+map, cv2.IMREAD_GRAYSCALE)
+    blur = cv2.GaussianBlur(image_gray, ksize=(3,3), sigmaX=0)
+    ret, thresh1 = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
+    edged = cv2.Canny(blur, 10, 250)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
+    closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
 
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
-closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
-cv2.imwrite('resources/convert/convert-1.jpg', closed)
-cv2.imshow('closed', closed)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imwrite('resources/convert/'+map, closed)
+
+file_list = os.listdir('resources/convert/')
+
+for map in file_list:
+    img = Image.open('resources/convert/'+map)
+
+    img_resize = img.resize((570, 520))
+    img_resize.save('resources/convert/'+map)
+
+    # img_resize_lanczos = img.resize((256, 256), Image.LANCZOS)
+    # img_resize_lanczos.save('data/dst/sample_pillow_resize_lanczos.jpg')
