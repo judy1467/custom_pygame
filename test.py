15edd73cd5264339ab15cb8e984369944e450b17
judy1 <judy1467@naver.com>
2022-08-12 16:05:08 +0900

    맵파일 외곽선 따기

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-123
+import cv2, os
+
+image_gray = cv2.imread('resources/images/map/map1.jpg', cv2.IMREAD_GRAYSCALE)
+
+file_list = os.listdir('resources/images/map/')
+print(file_list)
+
+blur = cv2.GaussianBlur(image_gray, ksize=(3,3), sigmaX=0)
+ret, thresh1 = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
+edged = cv2.Canny(blur, 10, 250)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
+closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+cv2.imwrite('resources/convert/convert-1.jpg', closed)
+cv2.imshow('closed', closed)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
